1ab9adca38a03d945313f8b5675d7872f58b7de8
matthew-gordon <laxredemption5525@gmail.com>
2017-03-09 11:51:21 -0700

    Push to github

--- dashAndDineMobile/constants.swift
+++ dashAndDineMobile/constants.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-let BASE_URL: String = "https://dineanddash.herokuapp.com/"
+let BASE_URL: String = "https://dashanddine.herokuapp.com/"
 let CLIENT_ID: String = "RoXkpIghY4Jstpcwsx0Hj2On59BYIavWgbkvvNZL"
 let CLIENT_SECRET: String = "ZjjDBNENXpGZXTF0FS7dDmlITjF2NM58TDP4A6xwCsNunVR1pjr2yqed1pKGfcBScS0i9LfNrZOow0Y8C8KwFyanK6C6ZIYWFaNh7quvHEy816dKDMN7K76NksSsryaM"
 
